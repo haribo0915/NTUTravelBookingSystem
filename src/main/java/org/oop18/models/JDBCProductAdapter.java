0bf424a71b273a74d7a8ddbccf58bfbe2dfb78fc
b06902047 <b06902047@csie.ntu.edu.tw>
2020-06-18 12:30:30 +0800

    JDBCProductAdapter completed(not tested)

--- src/main/java/org/oop18/models/JDBCProductAdapter.java
+++ src/main/java/org/oop18/models/JDBCProductAdapter.java
@@ -15,7 +15,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class JDBCProductAdapter implements ProductAdapter {
@@ -41,7 +43,7 @@ public class JDBCProductAdapter implements ProductAdapter {
     }
     
     @Override
-    //retrieve data by product id?
+    //retrieve data by product id
     public Product queryProduct(Integer id) throws QueryException {
         try {
             String query = String.format("SELECT * from product WHERE id=%d",id);
@@ -51,13 +53,14 @@ public class JDBCProductAdapter implements ProductAdapter {
     		}
             Integer product_id = rs.getInt("id");
            	Integer travel_code_id=rs.getInt("travel_code_id");
+           	String title =rs.getString("title");
            	String product_key=rs.getString("product_key");
            	Integer price=rs.getInt("price");
            	Timestamp start = rs.getTimestamp("start_date");
            	Timestamp end = rs.getTimestamp("end_date");
            	Integer lower_bound=rs.getInt("lower_bound");
            	Integer upper_bound=rs.getInt("upper_bound");
-           	String title =rs.getString("title");
+
            	return new Product(product_id, travel_code_id,title,product_key,price,start,end, lower_bound,upper_bound);         
         }catch (Exception ex) {
     		throw new QueryException(ex.getMessage());
@@ -69,16 +72,84 @@ public class JDBCProductAdapter implements ProductAdapter {
     //      you can take StubProductAdapter as reference.
     @Override
     public List<Product> queryProducts(TravelCode travelCode, Timestamp startDate) throws QueryException {
-        return null;
+    	List<Product> productList = new ArrayList<>();
+        try {
+            String query = String.format("SELECT * from product");
+            rs = st.executeQuery(query);
+    		if (rs.next() == false) {
+    			throw new QueryException("Product not found!");
+    		}  
+    		do {
+    			Integer product_id = rs.getInt("id");
+    			Integer travel_code_id=rs.getInt("travel_code_id");
+    			String title =rs.getString("title");
+    			String product_key=rs.getString("product_key");
+    			Integer price=rs.getInt("price");
+    			Timestamp start = rs.getTimestamp("start_date");
+    			Timestamp end = rs.getTimestamp("end_date");
+    			Integer lower_bound=rs.getInt("lower_bound");
+    			Integer upper_bound=rs.getInt("upper_bound");
+           	
+    			productList.add(new Product(product_id, travel_code_id,title,product_key,price,start,end, lower_bound,upper_bound));
+    		}
+    		while(rs.next());
+            if (travelCode != null) {
+                productList = productList.stream()
+                                    .filter((Product product) -> (product.getTravelCodeId().equals(travelCode.getId())))
+                                    .collect(Collectors.toList());
+            }
+            if (startDate != null) {
+                productList = productList.stream()
+                                    .filter((Product product) -> (product.getStartDate().equals(startDate)))
+                                    .collect(Collectors.toList());
+            }
+
+            return productList; 
+        }catch (Exception ex) {
+    		throw new QueryException(ex.getMessage());
+    	}    	
     }
 
     @Override
     public List<TravelCode> queryTravelCodes() throws QueryException {
-        return null;
+    	List<TravelCode> travelCodeList = new ArrayList<>();
+        try{
+            String query = String.format("SELECT * from travel_code");
+            rs = st.executeQuery(query);
+    		if (rs.next() == false) {
+    			throw new QueryException("Product not found!");
+    		}  
+    		do {
+    			Integer travel_code_id=rs.getInt("id");
+    			Integer travel_code =rs.getInt("travel_code");
+    			String travel_code_name=rs.getString("travel_code_name");
+           	
+    			travelCodeList.add(new TravelCode(travel_code_id, travel_code, travel_code_name));
+    		}
+    		while(rs.next());
+    		
+    		return travelCodeList;	 
+        }catch (Exception ex) {
+    		throw new QueryException(ex.getMessage());
+    	} 
     }
 
     @Override
     public TravelCode queryTravelCode(String travelCodeName) throws QueryException {
-        return null;
+        try{
+            String query = String.format("SELECT * from travel_code where travel_code_name=%s",travelCodeName);
+            rs = st.executeQuery(query);
+    		if (rs.next() == false) {
+    			throw new QueryException("Product not found!");
+    		}
+    		Integer travel_code_id=rs.getInt("id");
+    		Integer travel_code =rs.getInt("travel_code");
+    		String travel_code_name=rs.getString("travel_code_name");
+           	
+    		return(new TravelCode(travel_code_id, travel_code, travel_code_name));
+    		
+        }catch (Exception ex) {
+    		throw new QueryException(ex.getMessage());
+    	}
     }
 }
