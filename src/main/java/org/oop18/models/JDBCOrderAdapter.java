96215de9aa82b2e3fbd001f04832347f80e1a8f8
ChangOliver <b06902119@ntu.edu.tw>
2020-06-21 11:59:33 +0800

    space tab conversion

--- src/main/java/org/oop18/models/JDBCOrderAdapter.java
+++ src/main/java/org/oop18/models/JDBCOrderAdapter.java
@@ -13,12 +13,12 @@ import java.util.List;
 
 
 public class JDBCOrderAdapter implements OrderAdapter {
-	private JDBCConnectionPool jdbcConnectionPool;
+    private JDBCConnectionPool jdbcConnectionPool;
 
-	public JDBCOrderAdapter() {
-		jdbcConnectionPool = JDBCConnectionPool.getInstance();
-	}
-	
+    public JDBCOrderAdapter() {
+        jdbcConnectionPool = JDBCConnectionPool.getInstance();
+    }
+    
     @Override
     /**
     * Method to insert record to database.
@@ -29,11 +29,11 @@ public class JDBCOrderAdapter implements OrderAdapter {
     * @return order id is set. 
     */
     public Order createOrder(Order order) throws CreateException {
-		Connection conn = jdbcConnectionPool.takeOut();
+        Connection conn = jdbcConnectionPool.takeOut();
         try{
             // calculate and check # of people
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(String.format("SELECT * from product WHERE id=%d", order.getProductId()));
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(String.format("SELECT * from product WHERE id=%d", order.getProductId()));
             rs.next();
             if( rs.getInt("id") != order.getProductId() ) 
                 throw new CreateException("Invalid Product ID");
@@ -61,8 +61,8 @@ public class JDBCOrderAdapter implements OrderAdapter {
             long oneDay = 1 * 24 * 60 * 60 * 1000;
             Timestamp deadline = new Timestamp(start_date.getTime() - 10*oneDay);
             if( (order.getCreatedTime()).after(deadline) ) {
-				throw new CreateException("Cannot place orders for incoming 10 days.");
-			}
+                throw new CreateException("Cannot place orders for incoming 10 days.");
+            }
             
             // create order id : append to `order` table
             rs = st.executeQuery("SELECT COUNT(*) FROM `order`");
@@ -75,12 +75,12 @@ public class JDBCOrderAdapter implements OrderAdapter {
                 order.getAdultCount(), order.getChildrenCount(), order.getTotalPrice(), order.getCreatedTime() ));
         }
         catch(SQLException e){
-        	e.printStackTrace();
+            e.printStackTrace();
         }
-		finally {
-			jdbcConnectionPool.takeIn(conn);
-		}
-		return order;
+        finally {
+            jdbcConnectionPool.takeIn(conn);
+        }
+        return order;
     }
 
     @Override
@@ -90,46 +90,46 @@ public class JDBCOrderAdapter implements OrderAdapter {
     * @return order
     */
     public Order updateOrder(Order order) throws UpdateException, EntryNotFoundException {
-		Integer id = null;
-		Integer userId = null;
-		Integer productId = null;
-		Integer adultCount = null;
-		Integer childrenCount = null;
-		Integer totalPrice = null;
-		Timestamp createdTime = null;
+        Integer id = null;
+        Integer userId = null;
+        Integer productId = null;
+        Integer adultCount = null;
+        Integer childrenCount = null;
+        Integer totalPrice = null;
+        Timestamp createdTime = null;
 
-		Connection conn = jdbcConnectionPool.takeOut();
-    	
-    	try {
-    		// Query with order ID
-    		String query = String.format("SELECT * FROM `order` WHERE `id` = \'%d\'", order.getId());
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-    		if (rs.next() == false) {
+        Connection conn = jdbcConnectionPool.takeOut();
+        
+        try {
+            // Query with order ID
+            String query = String.format("SELECT * FROM `order` WHERE `id` = \'%d\'", order.getId());
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            if (rs.next() == false) {
                 //throw exception if no order is found
-    			throw new EntryNotFoundException("Order not found!");
-    		}
-    		
-    		Integer originalAdultCount = rs.getInt("adult_count");
-    		Integer originalChildrenCount = rs.getInt("children_count");
-    		
-    		//Query with product ID
-			query = String.format("SELECT * FROM `product` WHERE `id` = \'%d\'", order.getProductId());
-			rs = st.executeQuery(query);
-			
-    		if (rs.next() == false) {
+                throw new EntryNotFoundException("Order not found!");
+            }
+            
+            Integer originalAdultCount = rs.getInt("adult_count");
+            Integer originalChildrenCount = rs.getInt("children_count");
+            
+            //Query with product ID
+            query = String.format("SELECT * FROM `product` WHERE `id` = \'%d\'", order.getProductId());
+            rs = st.executeQuery(query);
+            
+            if (rs.next() == false) {
                 //throw exception if no product is found
-    			throw new EntryNotFoundException("Product not found!");
-    		}
+                throw new EntryNotFoundException("Product not found!");
+            }
 
-			//Check days between today and departure date
-			LocalDateTime departure = rs.getTimestamp("start_date").toLocalDateTime();
-			LocalDateTime today = LocalDateTime.now();
-			if (ChronoUnit.DAYS.between(today, departure) < 10 ) {
+            //Check days between today and departure date
+            LocalDateTime departure = rs.getTimestamp("start_date").toLocalDateTime();
+            LocalDateTime today = LocalDateTime.now();
+            if (ChronoUnit.DAYS.between(today, departure) < 10 ) {
                 //throw exception if order can't be changed
-				throw new UpdateException("You can only modify orders 10 days before departure!");
-			}
-			
+                throw new UpdateException("You can only modify orders 10 days before departure!");
+            }
+            
             
             if( rs.getInt("id") != order.getProductId() ) 
                 throw new UpdateException("Invalid Product ID");
@@ -152,33 +152,33 @@ public class JDBCOrderAdapter implements OrderAdapter {
             // throw exception if current guest exceeds upper bound
             if( (sum+order.getAdultCount()+order.getChildrenCount()-originalAdultCount-originalChildrenCount) > max_people )
                 throw new UpdateException( String.format("This trip handles at most %d people.\nAn order of %d people is placed.\nCurrently registered: %d.", max_people, order.getAdultCount()+order.getChildrenCount(), sum));
-			
-			//update order
-			query = String.format("UPDATE `order` SET `adult_count` = \'%d\', `children_count` = \'%d\' WHERE (`id` = \'%d\')", order.getAdultCount(), order.getChildrenCount(), order.getId());
-			st.executeUpdate(query);
-			
-			//make sure the order is updated
-			query = String.format("SELECT * FROM `order` WHERE `id` = \'%d\'", order.getId());
-			rs = st.executeQuery(query);
-    		if (rs.next() == false) {
-    			throw new EntryNotFoundException("Order not found!");
-    		}
-			
-    		id = rs.getInt("id");
-    		userId = rs.getInt("user_id");
-    		productId = rs.getInt("product_id");
-    		adultCount = rs.getInt("adult_count");
-    		childrenCount = rs.getInt("children_count");
-    		totalPrice = rs.getInt("total_price");
-    		createdTime = rs.getTimestamp("created_time");
-    	}
-    	catch (SQLException ex) {
-    		ex.printStackTrace();
-    	}
-		finally {
-			jdbcConnectionPool.takeIn(conn);
-		}
-		return new Order(id, userId, productId, adultCount, childrenCount, totalPrice, createdTime);
+            
+            //update order
+            query = String.format("UPDATE `order` SET `adult_count` = \'%d\', `children_count` = \'%d\' WHERE (`id` = \'%d\')", order.getAdultCount(), order.getChildrenCount(), order.getId());
+            st.executeUpdate(query);
+            
+            //make sure the order is updated
+            query = String.format("SELECT * FROM `order` WHERE `id` = \'%d\'", order.getId());
+            rs = st.executeQuery(query);
+            if (rs.next() == false) {
+                throw new EntryNotFoundException("Order not found!");
+            }
+            
+            id = rs.getInt("id");
+            userId = rs.getInt("user_id");
+            productId = rs.getInt("product_id");
+            adultCount = rs.getInt("adult_count");
+            childrenCount = rs.getInt("children_count");
+            totalPrice = rs.getInt("total_price");
+            createdTime = rs.getTimestamp("created_time");
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            jdbcConnectionPool.takeIn(conn);
+        }
+        return new Order(id, userId, productId, adultCount, childrenCount, totalPrice, createdTime);
     }
 
     @Override
@@ -188,63 +188,63 @@ public class JDBCOrderAdapter implements OrderAdapter {
     * @return order
     */
     public Order deleteOrder(Order order) throws EntryNotFoundException {
-		Integer id = null;
-		Integer userId = null;
-		Integer productId = null;
-		Integer adultCount = null;
-		Integer childrenCount = null;
-		Integer totalPrice = null;
-		Timestamp createdTime = null;
+        Integer id = null;
+        Integer userId = null;
+        Integer productId = null;
+        Integer adultCount = null;
+        Integer childrenCount = null;
+        Integer totalPrice = null;
+        Timestamp createdTime = null;
 
-		Connection conn = jdbcConnectionPool.takeOut();
-		
-    	try {
+        Connection conn = jdbcConnectionPool.takeOut();
+        
+        try {
             // Query by order id
-    		String query = String.format("SELECT * FROM `order` WHERE `id` = \'%d\'", order.getId());
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-    		
-    		if (rs.next() == false) {
+            String query = String.format("SELECT * FROM `order` WHERE `id` = \'%d\'", order.getId());
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            
+            if (rs.next() == false) {
                 //throw exception if no order is found
-    			throw new EntryNotFoundException("Order not found");
-    		}
-    		
-    		id = rs.getInt("id");
-    		userId = rs.getInt("user_id");
-    		productId = rs.getInt("product_id");
-    		adultCount = rs.getInt("adult_count");
-    		childrenCount = rs.getInt("children_count");
-    		totalPrice = rs.getInt("total_price");
-    		createdTime = rs.getTimestamp("created_time");	
-    		
-    		//Query with product ID
-			query = String.format("SELECT * FROM `product` WHERE `id` = \'%d\'", order.getProductId());
-			rs = st.executeQuery(query);
-			
-    		if (rs.next() == false) {
+                throw new EntryNotFoundException("Order not found");
+            }
+            
+            id = rs.getInt("id");
+            userId = rs.getInt("user_id");
+            productId = rs.getInt("product_id");
+            adultCount = rs.getInt("adult_count");
+            childrenCount = rs.getInt("children_count");
+            totalPrice = rs.getInt("total_price");
+            createdTime = rs.getTimestamp("created_time");  
+            
+            //Query with product ID
+            query = String.format("SELECT * FROM `product` WHERE `id` = \'%d\'", order.getProductId());
+            rs = st.executeQuery(query);
+            
+            if (rs.next() == false) {
                 //throw exception if no product is found
-    			throw new EntryNotFoundException("Product not found!");
-    		}
-    		
-			//Check days between today and departure date
-			LocalDateTime departure = rs.getTimestamp("start_date").toLocalDateTime();
-			LocalDateTime today = LocalDateTime.now();
-			if (ChronoUnit.DAYS.between(today, departure) < 10 ) {
+                throw new EntryNotFoundException("Product not found!");
+            }
+            
+            //Check days between today and departure date
+            LocalDateTime departure = rs.getTimestamp("start_date").toLocalDateTime();
+            LocalDateTime today = LocalDateTime.now();
+            if (ChronoUnit.DAYS.between(today, departure) < 10 ) {
                 //throw exception if order can't be changed
-				throw new UpdateException("You can only delete orders 10 days before departure!");
-			}	
-    		
+                throw new UpdateException("You can only delete orders 10 days before departure!");
+            }   
+            
             // Delete the order
-    		query = String.format("DELETE FROM `order` WHERE `id` = \'%d\'", order.getId());
-    		st.executeUpdate(query);
-    	}
-    	catch (SQLException ex) {
-    		ex.printStackTrace();
-    	}
-		finally {
-			jdbcConnectionPool.takeIn(conn);
-		}
-		return new Order(id, userId, productId, adultCount, childrenCount, totalPrice, createdTime);
+            query = String.format("DELETE FROM `order` WHERE `id` = \'%d\'", order.getId());
+            st.executeUpdate(query);
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            jdbcConnectionPool.takeIn(conn);
+        }
+        return new Order(id, userId, productId, adultCount, childrenCount, totalPrice, createdTime);
     }
 
     @Override
@@ -254,40 +254,40 @@ public class JDBCOrderAdapter implements OrderAdapter {
     * @return order
     */
     public List<Order> queryOrders(Integer userId) throws EntryNotFoundException {
-    	
-    	List<Order> queryResult = new ArrayList<>();
+        
+        List<Order> queryResult = new ArrayList<>();
 
-    	Connection conn = jdbcConnectionPool.takeOut();
-    	
-    	try {
+        Connection conn = jdbcConnectionPool.takeOut();
+        
+        try {
             // Query order by user id
-    		String query = String.format("SELECT * FROM `order` WHERE `user_id` = \'%d\'", userId);
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-    		
-    		if (rs.next() == false) {
-    			throw new EntryNotFoundException("You have not ordered anything yet!");
-    		}
-    		
+            String query = String.format("SELECT * FROM `order` WHERE `user_id` = \'%d\'", userId);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            
+            if (rs.next() == false) {
+                throw new EntryNotFoundException("You have not ordered anything yet!");
+            }
+            
             // Append all orders made by user
-    		do {
-	    		Integer id = rs.getInt("id");
-	    		Integer QuserId = rs.getInt("user_id");
-	    		Integer productId = rs.getInt("product_id");
-	    		Integer adultCount = rs.getInt("adult_count");
-	    		Integer childrenCount = rs.getInt("children_count");
-	    		Integer totalPrice = rs.getInt("total_price");
-	    		Timestamp createdTime = rs.getTimestamp("created_time");	
-	    		
-	    		queryResult.add(new Order(id, QuserId, productId, adultCount, childrenCount, totalPrice, createdTime));
-    		} while (rs.next());		 
-    	}
-    	catch (SQLException ex) {
-    		ex.printStackTrace();
-    	}
-		finally {
-			jdbcConnectionPool.takeIn(conn);
-		}
-		return queryResult;
+            do {
+                Integer id = rs.getInt("id");
+                Integer QuserId = rs.getInt("user_id");
+                Integer productId = rs.getInt("product_id");
+                Integer adultCount = rs.getInt("adult_count");
+                Integer childrenCount = rs.getInt("children_count");
+                Integer totalPrice = rs.getInt("total_price");
+                Timestamp createdTime = rs.getTimestamp("created_time");    
+                
+                queryResult.add(new Order(id, QuserId, productId, adultCount, childrenCount, totalPrice, createdTime));
+            } while (rs.next());         
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            jdbcConnectionPool.takeIn(conn);
+        }
+        return queryResult;
     }
 }
